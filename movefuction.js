ad0241b8ae7219a5b5aaf2cd348cc0e9caa13aa4
Cassie618 <jamescaudell89@yahoo.com>
2024-12-21 01:34:22 -0600

    Add files via upload

--- movefuction.js
+++ movefuction.js
@@ -1,20 +1,25 @@
 let interval;
-let image = document.getElementById("img/slothmeme.jpg");
-
+let image = document.getElementById("movableImage");
+ 
 function startMoving(direction){
-    stopMoving();
-
-
-}
-    interval = setInterval (() =>{
+     stopMoving();
+     interval = setInterval(() => {
+         
         let left = image.offsetLeft;
-
-        if (direction === `right`) left += 5;
-        if (direction === `left`) left -+5;
-
+        let top = image.offsetTop;
+   
+        if (direction === 'right') left += 5;
+        if (direction === 'left') left -= 5;
+        if (direction === 'up') top -= 5;
+        if (direction === 'down') top += 5;
+ 
+        // Apply new position
         image.style.left = `${left}px`;
-    },
-50);
+        image.style.top = `${top}px`;
+    }, 50);  
+}
+ 
+ 
 function stopMoving(){
-    clearInterval(interval);
+ clearInterval(interval);
 }
